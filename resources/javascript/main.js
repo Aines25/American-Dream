120378b7eff0b9554699fe365572058729c9a3c8
aines25 <smithchristian4@hotmail.com>
2022-04-06 23:56:16 +0100

    started to add function for randomised quote upon click

--- resources/javascript/main.js
+++ resources/javascript/main.js
@@ -31,3 +31,15 @@ let aText = new Array(
     }
      
     typewriter();
+
+
+    // Generate random quote
+
+   /* const randomFact = [
+    'A little over 8 million people live in New York City. That means 1 in every 38 people in the United States call the city home.', 
+    'Times Square is named after the New York Times. It was originally called Longacre Square until the Times moved there in 1904.', 
+    'New York City’s Federal Reserve Bank has the largest gold storage in the world. The vault is 80 feet below street level and contains $90 billion in gold.',
+    'More than 800 languages are spoken in New York City, making it the most linguistically diverse city in the world. 4 in 10 households speak a language other than English.'
+    ]
+
+const generateRandomFact = () => 
